28b6bd7081d4e4732ae3b61392d425c73cd36e09
James Hawkins <james.hawkins@ucsf.edu>
2016-02-24 14:14:04 -0800

    initial commit

---
+++ spill/spill.py
@@ -0,0 +1,27 @@
+class Project(object):
+    """docstring for Project"""
+    def __init__(self, project_directory):
+        super(Project, self).__init__()
+        self.project_directory = project_directory
+
+
+class App(object):
+    """docstring for App"""
+    def __init__(self, arg):
+        super(App, self).__init__()
+        self.arg = arg
+
+
+class Blueprint(object):
+    """docstring for Blueprint"""
+    def __init__(self, arg):
+        super(Blueprint, self).__init__()
+        self.arg = arg
+
+
+class Test(object):
+    """docstring for Blueprint"""
+    def __init__(self, arg):
+        super(Blueprint, self).__init__()
+        self.arg = arg
+
